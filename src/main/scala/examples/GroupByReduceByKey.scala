f9d3f2f17231f68da6cca45461e79270e5e5c980
Venkat <venkatasr.dokku@gmail.com>
2017-06-15 11:11:33 -0400

    Spark API Works

---
+++ src/main/scala/examples/GroupByReduceByKey.scala
@@ -0,0 +1,28 @@
+package examples
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by vdokku on 6/15/2017.
+  */
+object GroupByReduceByKey {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Spark-GroupByKey-ReduceByKey-Example").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+    val words = Array("a", "b", "b", "c", "d", "e", "a", "b", "b", "c", "d", "e", "b", "b", "c", "d", "e")
+    val wordPairsRDD = sc.parallelize(words).map(word => (word, 1))
+
+    val wordCountsWithReduce = wordPairsRDD
+      .reduceByKey(_ + _)
+      .collect()
+    wordCountsWithReduce.foreach(f => println(f))
+
+    //Avoid GroupByKey
+    println("Avoid GroupByKey")
+    val wordCountsWithGroup = wordPairsRDD
+      .groupByKey()
+      .map(t => (t._1, t._2.sum))
+      .collect()
+    wordCountsWithGroup.foreach(f => println(f))
+  }
+}
